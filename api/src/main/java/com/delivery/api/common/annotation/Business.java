2469b0cff0e171a2b2da384c35ac49a2b2a3667b
tjdgusdk <sha6639@naver.com>
2024-11-18 12:46:12 +0900

    user 도메인 기본 패키지 설정

--- api/src/main/java/com/delivery/api/common/annotation/Business.java
+++ api/src/main/java/com/delivery/api/common/annotation/Business.java
@@ -1,6 +1,7 @@
 package com.delivery.api.common.annotation;
 
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.lang.annotation.ElementType;
