e6505ebbcd888c6e65a8ba554c072455c12a13f5
tjdgusdk <sha6639@naver.com>
2024-11-18 12:49:30 +0900

    사용자 가입처리 서비스 로직 작성 중

---
+++ api/src/main/java/com/delivery/api/domain/user/controller/model/UserResponse.java
@@ -0,0 +1,13 @@
+package com.delivery.api.domain.user.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+}
