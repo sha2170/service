0ba5983034d4ffca865b5d5878ad9efb08aeb64e
tjdgusdk <sha6639@naver.com>
2024-11-18 12:48:45 +0900

    user 도메인 기본 패키지 설정

---
+++ db/src/main/java/com/delivery/db/user/UserRepository.java
@@ -0,0 +1,16 @@
+package com.delivery.db.user;
+
+import com.delivery.db.user.enums.UserStatus;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // select * from user where id = ? and status = ? order by id desc limit 1
+    Optional<UserEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, UserStatus status);
+
+    // select * from user where email = ? and password = ? and status = ? oder by id desc limit 1
+    Optional<UserEntity> findFirstByEmailAndPasswordAndStatusOrderByIdDesc(String email, String password, UserStatus status);
+}
