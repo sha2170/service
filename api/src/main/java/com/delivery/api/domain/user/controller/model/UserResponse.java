4f8a2465d473518f3e1c56c29d6cfcb63f69df9a
tjdgusdk <sha6639@naver.com>
2024-11-19 12:07:27 +0900

    사용자 서비스 로직 - 1.가입완료

--- api/src/main/java/com/delivery/api/domain/user/controller/model/UserResponse.java
+++ api/src/main/java/com/delivery/api/domain/user/controller/model/UserResponse.java
@@ -1,13 +1,32 @@
 package com.delivery.api.domain.user.controller.model;
 
+import com.delivery.db.user.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserResponse {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private UserStatus status;
+
+    private String address;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime lastLoginAt;
 }
