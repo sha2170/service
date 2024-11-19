6196918dd5ddd322e62c374b0b71e05c5f8f53b9
tjdgusdk <sha6639@naver.com>
2024-11-19 12:10:20 +0900

    사용자 서비스 로직 - 1.가입 완료

--- api/src/main/java/com/delivery/api/domain/user/converter/UserConverter.java
+++ api/src/main/java/com/delivery/api/domain/user/converter/UserConverter.java
@@ -4,6 +4,7 @@ import com.delivery.api.common.annotation.Converter;
 import com.delivery.api.common.error.ErrorCode;
 import com.delivery.api.common.exception.ApiException;
 import com.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import com.delivery.api.domain.user.controller.model.UserResponse;
 import com.delivery.db.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 
@@ -28,5 +29,23 @@ public class UserConverter {
                             .build();
                 })
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "UserRegisterRequest Null"));
+    }
+
+    public UserResponse toResponse(UserEntity userEntity) {
+        return Optional.ofNullable(userEntity)
+                .map(it ->{
+                    // to response
+                    return UserResponse.builder()
+                            .id(userEntity.getId())
+                            .name(userEntity.getName())
+                            .status(userEntity.getStatus())
+                            .email(userEntity.getEmail())
+                            .address(userEntity.getAddress())
+                            .registeredAt(userEntity.getRegisteredAt())
+                            .unregisteredAt(userEntity.getUnregisteredAt())
+                            .lastLoginAt(userEntity.getLastLoginAt())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "UserRegisteredRequest Null"));
     }
 }
