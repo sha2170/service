923070e7fc4eb674552860a192db486dcaa6b302
tjdgusdk <sha6639@naver.com>
2024-11-20 15:05:20 +0900

    사용자 도메인 개발 : JWT 토큰 서비스 로직 적용 및 사용자 로그인 토큰 발행 적용 - 진행 중

---
+++ api/src/main/java/com/delivery/api/domain/token/business/TokenBusiness.java
@@ -0,0 +1,9 @@
+package com.delivery.api.domain.token.business;
+
+import com.delivery.api.common.annotation.Business;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Business
+public class TokenBusiness {
+}
---
+++ api/src/main/java/com/delivery/api/domain/token/controller/model/TokenResponse.java
@@ -0,0 +1,23 @@
+package com.delivery.api.domain.token.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponse {
+
+    private String accessToken;
+
+    private LocalDateTime accessTokenExpiredAt;
+
+    private String refreshToken;
+
+    private LocalDateTime refreshTokenExpiredAt;
+}
