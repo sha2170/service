2a418e1b506863da36ba2114f92850b04f06d432
tjdgusdk <sha6639@naver.com>
2024-11-25 15:35:45 +0900

    store 도메인 개발 : store table / entity / repository 개발 완료. 서비스로직 기본 틀

--- api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
+++ api/src/main/java/com/delivery/api/domain/user/controller/UserApiController.java
@@ -1,8 +1,10 @@
 package com.delivery.api.domain.user.controller;
 
+import com.delivery.api.common.annotation.UserSession;
 import com.delivery.api.common.api.Api;
 import com.delivery.api.domain.user.business.UserBusiness;
 import com.delivery.api.domain.user.controller.model.UserResponse;
+import com.delivery.api.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,10 @@ public class UserApiController {
     private final UserBusiness userBusiness;
 
     @GetMapping("/me")
-    public Api<UserResponse> me() {
-
-        var requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
-        var userId = requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST);
-
-        var response = userBusiness.me(Long.parseLong(userId.toString()));
+    public Api<UserResponse> me(
+            @UserSession User user
+    ){
+        var response = userBusiness.me(user.getId());
         return Api.OK(response);
     }
 
