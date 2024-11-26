4329f3d4fffbbc971ecdccc0e2595479e87d5afb
tjdgusdk <sha6639@naver.com>
2024-11-26 13:43:27 +0900

    Store Menu 서비스 로직: Service/DTO/Converter/Business 작성

---
+++ api/src/main/java/com/delivery/api/domain/storemenu/controller/model/StoreMenuRegisterRequest.java
@@ -0,0 +1,32 @@
+package com.delivery.api.domain.storemenu.controller.model;
+
+import com.delivery.db.storemenu.enums.StoreMenuStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreMenuRegisterRequest {
+
+    @NotNull
+    private Long storeId;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @NotBlank
+    private String thumbnailUrl;
+
+}
---
+++ api/src/main/java/com/delivery/api/domain/storemenu/controller/model/StoreMenuResponse.java
@@ -0,0 +1,35 @@
+package com.delivery.api.domain.storemenu.controller.model;
+
+import com.delivery.db.storemenu.enums.StoreMenuStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreMenuResponse {
+
+    private Long id;
+
+    private Long storeId;
+
+    private String name;
+
+    private BigDecimal amount;
+
+    private StoreMenuStatus status;
+
+    private String thumbnailUrl;
+
+    private int likeCount;
+
+    private int sequence;
+}
