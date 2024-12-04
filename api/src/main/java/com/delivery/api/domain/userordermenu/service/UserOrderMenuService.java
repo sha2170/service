8f209d19a6f240df023d52c914f93aa12bb79b44
tjdgusdk <sha6639@naver.com>
2024-12-04 13:24:57 +0900

    User Order 사용자 주문처리

--- api/src/main/java/com/delivery/api/domain/userordermenu/service/UserOrderMenuService.java
+++ api/src/main/java/com/delivery/api/domain/userordermenu/service/UserOrderMenuService.java
@@ -1,5 +1,7 @@
 package com.delivery.api.domain.userordermenu.service;
 
+import com.delivery.api.common.error.ErrorCode;
+import com.delivery.api.common.exception.ApiException;
 import com.delivery.db.userordermenu.UserOrderMenuEntity;
 import com.delivery.db.userordermenu.UserOrderMenuRepository;
 import com.delivery.db.userordermenu.enums.UserOrderMenuStatus;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +20,16 @@ public class UserOrderMenuService {
 
     public List<UserOrderMenuEntity> getUserOrderMenu(Long userOrderId) {
         return userOrderMenuRepository.findAllByUserOrderIdAndStatus(userOrderId, UserOrderMenuStatus.REGISTERED);
+    }
+
+    public UserOrderMenuEntity order(
+            UserOrderMenuEntity userOrderMenuEntity
+    ){
+        return Optional.ofNullable(userOrderMenuEntity)
+                .map(it -> {
+                    it.setStatus(UserOrderMenuStatus.REGISTERED);
+                    return userOrderMenuRepository.save(it);
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
     }
 }
